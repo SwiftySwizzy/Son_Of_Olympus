2b32640065a635fddc28db0eed947cdca08d6b58
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-02-28 01:12:45 +0200

    added some basic menu design

--- son_of_olympus/app/src/main/java/com/son_of_olympus/MainActivity.java
+++ son_of_olympus/app/src/main/java/com/son_of_olympus/MainActivity.java
@@ -1,19 +1,25 @@
 package com.son_of_olympus;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+       // TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
     }
 
     /**
@@ -21,9 +27,4 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
