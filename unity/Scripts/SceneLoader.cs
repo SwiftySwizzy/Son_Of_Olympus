d7d7d9a6be2bb08b15b384507ca64aa1c0b4dd4f
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-03-21 22:14:52 +0200

    Add files via upload

---
+++ unity/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void Play(){
+		SceneManager.LoadScene ("main_scene");
+	}
+
+	public void Shop() {
+	
+	}
+
+	public void Exit () {
+
+	}
+
+	public void SetMusicVolume(float value) {
+	}
+}
