024f47b86b004c671c5b1baebbd3bfab57f30581
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-03-27 11:57:05 +0300

    Add files via upload

---
+++ unity/Scripts/stairsTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stairsTrigger : MonoBehaviour {
+	public Transform StairsPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider yayother) {
+		Instantiate(StairsPrefab, new Vector3(-183, transform.parent.position.y + 20f, transform.parent.position.z + 40), StairsPrefab.rotation);
+
+		transform.parent.gameObject.AddComponent<TimeToDestroy> ();
+	}
+}
